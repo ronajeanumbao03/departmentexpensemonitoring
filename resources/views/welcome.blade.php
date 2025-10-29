29d15348551681518a85ac4261cd382d62371f0b
rona <ronajeanumbao03@gmai.com>
2025-10-29 12:30:34 +0800

    asss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-gwapo
+gwaposadadsasd
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
